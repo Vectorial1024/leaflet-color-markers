d898777ffd341fe0a1dcc35d12a2f8cc5b924c75
Vincent Wong <vincentwwy168@gmail.com>
2026-01-15 16:20:25 +0800

    Suppress known Leaflet "errors"

--- rollup-demo.config.js
+++ rollup-demo.config.js
@@ -15,6 +15,21 @@ export default {
             sourcemap: true,
 		}
 	],
+    onwarn(warning, warn) {
+        if (warning.code === 'CIRCULAR_DEPENDENCY') {
+            // Leaflet 1 is constructed circularly, this is normal
+            return;
+        }
+        if (warning.code === 'MISSING_EXPORT') {
+            // Leaflet 1 works by doing `export.x = x`, this is normal
+            return;
+        }
+        if (warning.code === 'THIS_IS_UNDEFINED') {
+            // Leaflet 1 is not using legacy class syntax and therefore manually binds to `this`, this is normal
+            return;
+        }
+        warn(warning);
+    },
     plugins: [
         nodeResolve({
             jsnext: true,
