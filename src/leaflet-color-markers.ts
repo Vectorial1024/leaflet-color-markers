0f3df82497895f55a36658459e5d0cd2ba6d2261
Vincent Wong <vincentwwy168@gmail.com>
2026-01-17 12:27:28 +0800

    Extract to common function

--- src/leaflet-color-markers.ts
+++ src/leaflet-color-markers.ts
@@ -1,106 +1,19 @@
-import * as L from 'leaflet';
+import { makeColoredMarkerIcon } from './utils.js';
 
-const iconSize = [25, 41] as L.PointTuple;
-const iconAnchor = [12, 41] as L.PointTuple;
-const popupAnchor = [1, -34] as L.PointTuple;
-const tooltipAnchor = [16, -28] as L.PointTuple;
-const shadowSize = [41, 41] as L.PointTuple;
+export const blueIcon = makeColoredMarkerIcon('img/marker-icon-blue.png', 'img/marker-icon-2x-blue.png');
 
-export const blueIcon = new L.Icon({
-    iconUrl: 'img/marker-icon-blue.png',
-    iconRetinaUrl: 'img/marker-icon-2x-blue.png',
-    shadowUrl: 'img/marker-shadow.png',
-    iconSize: iconSize,
-    iconAnchor: iconAnchor,
-    popupAnchor: popupAnchor,
-    tooltipAnchor: tooltipAnchor,
-    shadowSize: shadowSize
-});
+export const goldIcon = makeColoredMarkerIcon('img/marker-icon-gold.png', 'img/marker-icon-2x-gold.png');
 
-export const goldIcon = new L.Icon({
-    iconUrl: 'img/marker-icon-gold.png',
-    iconRetinaUrl: 'img/marker-icon-2x-gold.png',
-    shadowUrl: 'img/marker-shadow.png',
-    iconSize: iconSize,
-    iconAnchor: iconAnchor,
-    popupAnchor: popupAnchor,
-    tooltipAnchor: tooltipAnchor,
-    shadowSize: shadowSize
-});
+export const redIcon = makeColoredMarkerIcon('img/marker-icon-red.png', 'img/marker-icon-2x-red.png');
 
-export const redIcon = new L.Icon({
-    iconUrl: 'img/marker-icon-red.png',
-    iconRetinaUrl: 'img/marker-icon-2x-red.png',
-    shadowUrl: 'img/marker-shadow.png',
-    iconSize: iconSize,
-    iconAnchor: iconAnchor,
-    popupAnchor: popupAnchor,
-    tooltipAnchor: tooltipAnchor,
-    shadowSize: shadowSize
-});
+export const greenIcon = makeColoredMarkerIcon('img/marker-icon-green.png', 'img/marker-icon-2x-green.png');
 
-export const greenIcon = new L.Icon({
-    iconUrl: 'img/marker-icon-green.png',
-    iconRetinaUrl: 'img/marker-icon-2x-green.png',
-    shadowUrl: 'img/marker-shadow.png',
-    iconSize: iconSize,
-    iconAnchor: iconAnchor,
-    popupAnchor: popupAnchor,
-    tooltipAnchor: tooltipAnchor,
-    shadowSize: shadowSize
-});
+export const orangeIcon = makeColoredMarkerIcon('img/marker-icon-orange.png', 'img/marker-icon-2x-orange.png');
 
-export const orangeIcon = new L.Icon({
-    iconUrl: 'img/marker-icon-orange.png',
-    iconRetinaUrl: 'img/marker-icon-2x-orange.png',
-    shadowUrl: 'img/marker-shadow.png',
-    iconSize: iconSize,
-    iconAnchor: iconAnchor,
-    popupAnchor: popupAnchor,
-    tooltipAnchor: tooltipAnchor,
-    shadowSize: shadowSize
-});
+export const yellowIcon = makeColoredMarkerIcon('img/marker-icon-yellow.png', 'img/marker-icon-2x-yellow.png');
 
-export const yellowIcon = new L.Icon({
-    iconUrl: 'img/marker-icon-yellow.png',
-    iconRetinaUrl: 'img/marker-icon-2x-yellow.png',
-    shadowUrl: 'img/marker-shadow.png',
-    iconSize: iconSize,
-    iconAnchor: iconAnchor,
-    popupAnchor: popupAnchor,
-    tooltipAnchor: tooltipAnchor,
-    shadowSize: shadowSize
-});
+export const violetIcon = makeColoredMarkerIcon('img/marker-icon-violet.png', 'img/marker-icon-2x-violet.png');
 
-export const violetIcon = new L.Icon({
-    iconUrl: 'img/marker-icon-violet.png',
-    iconRetinaUrl: 'img/marker-icon-2x-violet.png',
-    shadowUrl: 'img/marker-shadow.png',
-    iconSize: iconSize,
-    iconAnchor: iconAnchor,
-    popupAnchor: popupAnchor,
-    tooltipAnchor: tooltipAnchor,
-    shadowSize: shadowSize
-});
+export const greyIcon = makeColoredMarkerIcon('img/marker-icon-grey.png', 'img/marker-icon-2x-grey.png');
 
-export const greyIcon = new L.Icon({
-    iconUrl: 'img/marker-icon-grey.png',
-    iconRetinaUrl: 'img/marker-icon-2x-grey.png',
-    shadowUrl: 'img/marker-shadow.png',
-    iconSize: iconSize,
-    iconAnchor: iconAnchor,
-    popupAnchor: popupAnchor,
-    tooltipAnchor: tooltipAnchor,
-    shadowSize: shadowSize
-});
-
-export const blackIcon = new L.Icon({
-    iconUrl: 'img/marker-icon-black.png',
-    iconRetinaUrl: 'img/marker-icon-2x-black.png',
-    shadowUrl: 'img/marker-shadow.png',
-    iconSize: iconSize,
-    iconAnchor: iconAnchor,
-    popupAnchor: popupAnchor,
-    tooltipAnchor: tooltipAnchor,
-    shadowSize: shadowSize
-});
+export const blackIcon = makeColoredMarkerIcon('img/marker-icon-black.png', 'img/marker-icon-2x-black.png');
