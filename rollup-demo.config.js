ff2aca57ee02cc8f0b44ca88764970db936eb869
Vincent Wong <vincentwwy168@gmail.com>
2026-01-21 01:14:39 +0800

    Use TS for demo app

--- rollup-demo.config.js
+++ rollup-demo.config.js
@@ -9,7 +9,7 @@ import commonjs from "@rollup/plugin-commonjs";
 import url from "@rollup/plugin-url";
 
 export default {
-    input: 'demoApp.js',
+    input: 'demoApp.ts',
     output: [
         {
             file: 'dist/demoBundle.js',
